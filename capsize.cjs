148461be4d540a74c30de8749eeb53dd0f403874
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2023-11-30 16:09:45 -0300

    upkeep

--- capsize.cjs
+++ capsize.cjs
@@ -1,34 +1,34 @@
 const capsizeCssCore = require("@capsizecss/core");
 const fontMetrics = require("@capsizecss/metrics/sourceCodePro");
 
-let capsizeOptions = {
+const capsizeOptions = {
   capHeight: 16,
-  leading: 24,
+  leading: 16 * 1.25,
   fontMetrics,
 };
 
 console.log(JSON.stringify(capsizeCssCore.precomputeValues(capsizeOptions)));
 console.log(
-  capsizeCssCore.createStyleString("sourceCodeProCapsize", capsizeOptions)
+  capsizeCssCore.createStyleString("capsizedText", capsizeOptions),
 );
 
 /*
-{"fontSize":"24.2424px","lineHeight":"24px","capHeightTrim":"-0.1905em","baselineTrim":"-0.1395em"}
+{"fontSize":"24.2424px","lineHeight":"20px","capHeightTrim":"-0.108em","baselineTrim":"-0.057em"}
 
 .capsizedText {
   font-size: 24.2424px;
-  line-height: 24px;
+  line-height: 20px;
 }
 
 .capsizedText::before {
   content: "";
-  margin-bottom: -0.1905em;
+  margin-bottom: -0.108em;
   display: table;
 }
 
 .capsizedText::after {
   content: "";
-  margin-top: -0.1395em;
+  margin-top: -0.057em;
   display: table;
 }
 */
